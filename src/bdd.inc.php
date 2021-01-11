a29dd474077a65eb09652cf8887f97b685f24fd7
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-01-11 10:32:48 +0100

    almost done

---
+++ src/bdd.inc.php
@@ -0,0 +1,35 @@
+<?php
+$host = 'localhost';
+$db   = 'vacances';
+$user = 'Nicolas';
+$pass = 'Nicolas';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+// Selectionner la base
+try { // On se connecte à MySQL
+    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+    $bdd = new PDO($dsn, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', $pdo_options));
+
+    // On récupère tout le contenu   
+    $reponse = $bdd->query('SELECT * FROM `offre`');
+
+    // On affiche chaque entrée une à une
+    while ($donnee = $reponse->fetch()) {
+        echo '  <li>
+            <figure>
+                <img src="' . $donnee['image'] . '" alt="offre">
+                <figcaption>
+                    <h3>'. $donnee['titre'].'</h3>
+                    <p>'.$donnee['contenu'].'
+                    </p>
+                </figcaption>
+            </figure>
+        </li>
+            ';
+    }
+    $reponse->closeCursor(); // Terminer le traitement
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
---
+++ src/main.inc.php
@@ -0,0 +1,8 @@
+<main>
+        <section id="guide">
+            <h2>Guide des stations</h2>
+            <ul>
+                <?php include('./src/bdd.inc.php') ?>
+            </ul>
+        </section>
+    </main>
