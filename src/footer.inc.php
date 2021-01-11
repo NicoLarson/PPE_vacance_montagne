fe56066afb2ce893273199bee2a71631f42e5aac
Nicolas Yang <nicolas.yang.1@gmail.com>
2021-01-11 12:44:20 +0100

    footer + mailto

--- src/footer.inc.php
+++ src/footer.inc.php
@@ -1,10 +1,12 @@
 <footer id="reservation">
-        <h2>Réservations</h2>
-        <ul>
-            <li><i class="fas fa-map-marker"></i> Paris</li>
-            <li><i class="fas fa-phone"></i> Phone: +33 07 11 24 33 00</li>
-            <li><i class="fas fa-envelope"></i> Email: mail@mail.com</li>
-            <li><a href="#"><i class="fas fa-caret-up"></i> Retour à l'acceuil</a></li>
-        </ul>
-        <p>&copy; Design by MIT <?php print date('Y')?>   </p>
-    </footer>
+    <h2>Réservations</h2>
+    <ul>
+        <li><i class="fas fa-map-marker"></i> Paris</li>
+        <li><i class="fas fa-phone"></i> Phone: +33 07 11 24 33 00</li>
+        <li>
+            <a href="mailto:mail@mail.com"><i class="fas fa-envelope"></i> Email: mail@mail.com</a>
+        </li>
+        <li><a href="#"><i class="fas fa-caret-up"></i> Retour à l'acceuil</a></li>
+    </ul>
+    <p>&copy; Design by MIT <?php print date('Y') ?> </p>
+</footer>
